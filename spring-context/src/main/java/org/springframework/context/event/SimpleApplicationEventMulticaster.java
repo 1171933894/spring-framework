ae9964b4518b7cf5b32df66ce2323674c603a95b
1171933894@qq.com <1171933894@qq.com>
2020-03-28 21:28:44 +0800

    提交测试项目

--- spring-context/src/main/java/org/springframework/context/event/SimpleApplicationEventMulticaster.java
+++ spring-context/src/main/java/org/springframework/context/event/SimpleApplicationEventMulticaster.java
@@ -127,6 +127,11 @@ public class SimpleApplicationEventMulticaster extends AbstractApplicationEventM
 		multicastEvent(event, resolveDefaultEventType(event));
 	}
 
+	/**
+	 * 当产生 Spring 事件的时候会默认使用 SimpleApplicationEventMulticaster 的
+	 * multicastEvent 来广播事件，遍历所有监听器，并使用监听器中的 onApplicationEvent 方法来进
+	 * 行监听器的处理 而对于每个监听器来说其实都可以获取到产生的事件，但是是否进行处理则由事件监听器来决定
+	 */
 	@Override
 	public void multicastEvent(final ApplicationEvent event, @Nullable ResolvableType eventType) {
 		ResolvableType type = (eventType != null ? eventType : resolveDefaultEventType(event));
