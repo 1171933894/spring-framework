2cb3882913174bebb6ce3bd2b7c03a1ebe14de7c
heyuanxin3 <heyuanxin3@jd.com>
2020-04-16 21:08:32 +0800

    commit

--- spring-core/src/main/java/org/springframework/core/Ordered.java
+++ spring-core/src/main/java/org/springframework/core/Ordered.java
@@ -20,7 +20,7 @@ package org.springframework.core;
  * {@code Ordered} is an interface that can be implemented by objects that
  * should be <em>orderable</em>, for example in a {@code Collection}.
  *
- * <p>The actual {@link #getOrder() order} can be interpreted as prioritization,
+ * <p>The actual {@link #getOrder() order} can be interpreted as prioritization（优先次序）,
  * with the first object (with the lowest order value) having the highest
  * priority.
  *
@@ -43,7 +43,7 @@ package org.springframework.core;
 public interface Ordered {
 
 	/**
-	 * Useful constant for the highest precedence value.
+	 * Useful constant for the highest precedence（优先权）value.
 	 * @see java.lang.Integer#MIN_VALUE
 	 */
 	int HIGHEST_PRECEDENCE = Integer.MIN_VALUE;
@@ -60,7 +60,7 @@ public interface Ordered {
 	 * <p>Higher values are interpreted as lower priority. As a consequence,
 	 * the object with the lowest value has the highest priority (somewhat
 	 * analogous to Servlet {@code load-on-startup} values).
-	 * <p>Same order values will result in arbitrary sort positions for the
+	 * <p>Same order values will result in arbitrary（任意的）sort positions for the
 	 * affected objects.
 	 * @return the order value
 	 * @see #HIGHEST_PRECEDENCE
